35f2b70822f51aeca606af80daeb2acd27db1358
Maksym <makslesk64@gmail.com>
2025-08-15 12:45:51 +0300

    some changes to upload user profile image

--- django_learn/users/views.py
+++ django_learn/users/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -16,6 +18,14 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
+@login_required
+def profile(request):
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
 
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
 
-
+    return render(request, 'users/profile.html', context)
