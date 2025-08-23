74a33f1d342d094c102f5a0b79efab07e0c80fb9
Maksym <makslesk64@gmail.com>
2025-08-23 21:41:16 +0300

    New feature 'Posts CRUD operations (CREATE, READ, UPDATE, DELETE)'

--- django_learn/users/views.py
+++ django_learn/users/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import logout
 
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
